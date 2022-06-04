fdf11050636f2e4a15df408b738200811b008acd
Gustavo <gustavofelix01@yahoo.com>
2022-06-03 21:40:29 -0300

    polimorfismo
    Ppolimorfismo
    Relacionamento é um
    Representação textual de um objeto

--- Alura 3/modelo.py
+++ Alura 3/modelo.py
@@ -19,15 +19,25 @@ class Programa:
     def nome(self, nome):
         self._nome = nome.title()
 
+    def __str__(self): #__str__ altera o comando print referente a funcao
+        return(f'Nome: {self.nome} Ano {self.ano} Likes: {self.likes}')
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return(f'Nome: {self.nome} Ano {self.ano} Duracao {self.duracao} Likes: {self.likes}')
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+    
+    def __str__(self): #def imprime
+        return(f'Nome: {self.nome} Ano {self.ano} temporadas {self.temporadas} Likes: {self.likes}')
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -38,5 +48,12 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
+"""
 print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
 print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+"""
+lista = [atlanta, vingadores]
+
+for programa in lista:
+    #print(f'Nome: {programa.nome} - Likes: {programa.likes}')
+    print(programa)
