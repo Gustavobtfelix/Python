7972b4352cee33be6c830f5140fa63fbda2cb995
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-06-07 11:49:35 -0300

    class playlist

--- Alura 3/modelo.py
+++ Alura 3/modelo.py
@@ -38,9 +38,25 @@ class Serie(Programa):
     def __str__(self): #def imprime
         return(f'Nome: {self.nome} Ano {self.ano} temporadas {self.temporadas} Likes: {self.likes}')
 
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
+demolidor = Serie('demolidor', 2016, 2)
+tmep = Filme('todo mundo em panico', 1999, 100)
 vingadores.dar_likes()
 vingadores.dar_likes()
 vingadores.dar_likes()
@@ -52,8 +68,11 @@ atlanta.dar_likes()
 print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
 print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
 """
-lista = [atlanta, vingadores]
+lista = [atlanta, vingadores, demolidor, tmep]
+minha_playlist = Playlist('fim de semana', lista)
 
-for programa in lista:
+for programa in minha_playlist.listagem:
     #print(f'Nome: {programa.nome} - Likes: {programa.likes}')
     print(programa)
+
+print(f'Tamanho: {len(minha_playlist.listagem)}')
