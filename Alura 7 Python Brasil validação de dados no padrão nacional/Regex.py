8ab2a0c51c2c35a18bb1c67b50008840c8a68e86
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-11-17 15:59:01 -0300

    Criando Regex para e-mail com padrão brasileiro

--- Alura 7 Python Brasil validação de dados no padrão nacional/Regex.py
+++ Alura 7 Python Brasil validação de dados no padrão nacional/Regex.py
@@ -1,6 +1,11 @@
 import re
 
-padrao = "[0-9][a-z]{2}[0-9]" #numero, letra e quantidade, numero
-texto = "123 1ab2 1cc aa1"
-resposta = re.search (padrao, texto) #busca
+# padrao = "[0-9][a-z]{2}[0-9]" #numero, letra e quantidade, numero
+# texto = "123 1ab2 1cc aa1"
+# resposta = re.search (padrao, texto) #busca
+# print (resposta.group())
+
+padrao = "\w{5,50}@[a-z]{3,10}.com(.br)?"
+texto = "aaabbbcc rodrigo123@gmail.com.br uyfbsufbsfbbff"
+resposta = re.search(padrao, texto)
 print (resposta.group())
