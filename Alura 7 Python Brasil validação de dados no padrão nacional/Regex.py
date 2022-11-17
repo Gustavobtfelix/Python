27334ae106156a9c3f5beff8a7f97ee5ab988007
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-11-17 16:40:02 -0300

    Adicionando classe para validar telefones

--- Alura 7 Python Brasil validação de dados no padrão nacional/Regex.py
+++ Alura 7 Python Brasil validação de dados no padrão nacional/Regex.py
@@ -5,7 +5,17 @@ import re
 # resposta = re.search (padrao, texto) #busca
 # print (resposta.group())
 
-padrao = "\w{5,50}@[a-z]{3,10}.com(.br)?"
-texto = "aaabbbcc rodrigo123@gmail.com.br uyfbsufbsfbbff"
-resposta = re.search(padrao, texto)
-print (resposta.group())
+# #padrao email br
+# padrao = "\w{5,50}@[a-z]{3,10}.com(.br)?" #valor entre 5 e 50 digitos, @, letra entre 3 e 10, .com e .br opcional
+# texto = "aaabbbcc rodrigo123@gmail.com.br uyfbsufbsfbbff"
+# resposta = re.search(padrao, texto)
+# print (resposta.group())
+
+
+#padrao telefone fixo e celular
+#padrao = "[0-9]{2}[0-9]{4,5}[0-9]{4}"
+padrao = "[(]?[0-9]{2}[)]?[0-9]{4,5}[-]?[0-9]{4}"
+texto = "eu gosto do numero 2126451234 e gosto também do (21)93643-1234"
+resposta = re.findall(padrao, texto)
+
+print(resposta)
