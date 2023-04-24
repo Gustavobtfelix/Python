3f2a6b129b37803df67fb5622900183e0d1abdfc
Gustavo Batista Felix <gustavofelix01@yahoo.com>
2023-04-24 12:09:05 -0300

    continuando tdd

--- Alura 8 Python e TDD/main.py
+++ Alura 8 Python e TDD/main.py
@@ -1,6 +1,11 @@
 from codigo.bytebank import Funcionario
 
 
-ana = Funcionario('Ana', '12/03/1997', 1000)
+ana = Funcionario('Ana Carvalho Bourbon', '12/03/1997', 10000)
 
+print(ana.idade())
+print(ana.sobrenome())
+print(ana._eh_socio())
+print(ana.decrescimo_salario())
 print(ana.calcular_bonus())
+print(ana)
--- Alura 8 Python e TDD/tests/test_bytebank.py
+++ Alura 8 Python e TDD/tests/test_bytebank.py
@@ -2,32 +2,34 @@ from codigo.bytebank import Funcionario
 import pytest
 from pytest import mark
 
+
 class TestClass:
     def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
-        entrada = '13/03/2000' # Given-Contexto
+        entrada = '13/03/2000'  # Given-Contexto
         esperado = 22
 
         funcionario_teste = Funcionario('Teste', entrada, 1111)
-        resultado = funcionario_teste.idade() # When-ação
+        resultado = funcionario_teste.idade()  # When-ação
 
         assert resultado == esperado  # Then-desfecho
 
     def test_quando_sobrenome_recebe_Lucas_Carvalho_deve_retornar_Carvalho(self):
-        entrada = ' Lucas Carvalho ' # Given
+        entrada = ' Lucas Carvalho '  # Given
         esperado = 'Carvalho'
 
         lucas = Funcionario(entrada, '11/11/2000', 1111)
-        resultado = lucas.sobrenome() # When
+        resultado = lucas.sobrenome()  # When
 
         assert resultado == esperado
 
     def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
-        entrada_salario = 100000 #given
+        entrada_salario = 100000  # given
         entrada_nome = 'Paulo Bragança'
         esperado = 90000
 
-        funconario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
-        funconario_teste.decrescimo_salario() # when
+        funconario_teste = Funcionario(
+            entrada_nome, '11/11/2000', entrada_salario)
+        funconario_teste.decrescimo_salario()  # when
         resultado = funconario_teste.salario
 
         assert resultado == esperado  # then
@@ -38,7 +40,7 @@ class TestClass:
         esperado = 100
 
         funconario_teste = Funcionario('teste', '11/11/2000', entrada)
-        resultado = funconario_teste.calcular_bonus() # when
+        resultado = funconario_teste.calcular_bonus()  # when
 
         assert resultado == esperado  # then
 
@@ -51,16 +53,3 @@ class TestClass:
             resultado = funconario_teste.calcular_bonus()  # when
 
             assert resultado  # then
-
-
-
-
-
-
-
-
-
-
-
-
-
