854b620e761b561d8bd1bed9562b68cbc74e57ee
Gustavo Batista Felix <gustavofelix01@yahoo.com>
2023-05-12 11:19:32 -0300

    Adicionando fonte personalizada

--- manipulando imagens/adicionandoTexto.py
+++ manipulando imagens/adicionandoTexto.py
@@ -1,33 +1,16 @@
-# Python program to explain cv2.putText() method
-	
-# importing cv2
-import cv2
-	
-# Reading an image in default mode
-image = cv2.imread('geeks.png')
-	
-# Window name in which image is displayed
-window_name = 'Image'
+from PIL import Image, ImageDraw, ImageFont
 
-# font
-font = cv2.FONT_HERSHEY_SIMPLEX
+fontsize = 14
 
-# org
-org = (50, 50)
+font = ImageFont.truetype('./fontes/SHARONSANS-LIGHT.OTF', fontsize, encoding='utf-8')
+fontBold = ImageFont.truetype('./fontes/SHARONSANS-BOLD.OTF', fontsize, encoding='utf-8')
 
-# fontScale
-fontScale = 1
 
-# Blue color in BGR
-color = (255, 0, 0)
-
-# Line thickness of 2 px
-thickness = 2
-
-# Using cv2.putText() method
-image = cv2.putText(image, 'OpenCV', org, font,
-				fontScale, color, thickness, cv2.LINE_AA)
-
-# Displaying the image
-cv2.imshow(window_name, image)
-cv2.waitKey(0)
+# Create a new image onto which the text will be added
+image = Image.new(mode='RGB', size=(613, 953), color='#ffaa00')
+# Create an ImageDraw object onto which the font text will be placed
+draw = ImageDraw.Draw(im=image)
+# Draw the text onto our image
+draw.text(xy=(512, 128), text="Gustavo Batista Felix", font=font, fill='black', anchor='mm')
+# Open the image via system standard image viewer
+image.show()
