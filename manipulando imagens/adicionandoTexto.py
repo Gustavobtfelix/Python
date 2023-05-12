ed0cbe6939e3f2c3569d5890094512921a3a60b6
Gustavo Batista Felix <gustavofelix01@yahoo.com>
2023-05-12 11:38:18 -0300

    Centralizando texto horizontalmente

--- manipulando imagens/adicionandoTexto.py
+++ manipulando imagens/adicionandoTexto.py
@@ -1,16 +1,26 @@
 from PIL import Image, ImageDraw, ImageFont
 
-fontsize = 14
+fontsize = 28
 
 font = ImageFont.truetype('./fontes/SHARONSANS-LIGHT.OTF', fontsize, encoding='utf-8')
 fontBold = ImageFont.truetype('./fontes/SHARONSANS-BOLD.OTF', fontsize, encoding='utf-8')
 
-
 # Create a new image onto which the text will be added
 image = Image.new(mode='RGB', size=(613, 953), color='#ffaa00')
+
 # Create an ImageDraw object onto which the font text will be placed
-draw = ImageDraw.Draw(im=image)
+draw = ImageDraw.Draw(image)
+
+# Define the text and font properties
+text = "Gustavo"
+text_width, text_height = draw.textsize(text, font=font)
+
+# Calculate the x coordinates for centering the text
+x = (image.width - (text_width // 2)) // 2
+y = 128
+
 # Draw the text onto our image
-draw.text(xy=(512, 128), text="Gustavo Batista Felix", font=font, fill='black', anchor='mm')
+draw.text(xy=(x, y), text=text, font=font, fill='black', anchor='mm')
+
 # Open the image via system standard image viewer
 image.show()
