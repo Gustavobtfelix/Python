8e5cada19e422d682d4f8be9fdefa485d62e4b5e
Gustavo Batista Felix <gustavofelix01@yahoo.com>
2023-08-24 12:49:00 -0300

    adicionando metodo de alterar

--- Decorator/DatabaseDecorator.py
+++ Decorator/DatabaseDecorator.py
@@ -1,5 +1,5 @@
 import pyodbc
-
+#Cria conexao com o banco de dados
 def conexao(dados_conexao):
     try:
         global conecta
@@ -7,36 +7,41 @@ def conexao(dados_conexao):
         print('OK! conexao estabelecida')
         return True
     except Exception as e:
-        print('Não foi possível conectar no banco de dados. Abortando.')
+        print('Não foi possível conectar no banco de dados.')
         print(e)
         return False
 
-
+#Finaliza conexao com o banco de dados
 def finalizaConexao():
     conecta.close()
     print('Conexão finalizada')
 
-
+#Decorator banco de dados: Acessa banco de dados > Executa query > Retorna resultado
 def acessaDatabase(function):
     def wrapper(*args):
         argumentos = args[0]
+        print(argumentos)
         try:
             if conexao(argumentos['dados']):
-                result = function(argumentos['query'])
+                result = function(argumentos['query'], argumentos['parameters'])
                 finalizaConexao()
-                return result
+                if result is None:
+                    return None, 404
+                return result, 200
             else:
-                pass
+                return None, 599
         except Exception as e:
             finalizaConexao()
+            return None, 406
     return wrapper
 
+#Faz a busca de multiplos dados no banco de dados
 @acessaDatabase
-def buscaDadosMultiplos(query):
+def buscaDadosMultiplos(query, parameters):
     try:
         result_list = []
         cursor = conecta.cursor()
-        cursor.execute(query)
+        cursor.execute(query, parameters)
         rows = cursor.fetchall()
         # Assuming 'cursor.description' contains column names
         column_names = [desc[0] for desc in cursor.description]
@@ -51,12 +56,13 @@ def buscaDadosMultiplos(query):
         print(e)
         raise
 
+#Faz a busca de um dado no banco de dados
 @acessaDatabase
-def buscaDadoUnico(query):
+def buscaDadoUnico(query, parameters):
     try:
         result_list = {}
         cursor = conecta.cursor()
-        cursor.execute(query)
+        cursor.execute(query, parameters)
         row = cursor.fetchone()
         if row: 
             for idx, column in enumerate(cursor.description):
@@ -68,20 +74,35 @@ def buscaDadoUnico(query):
         print('erro ao executar query unica')
         print(e)
         raise
+
+#Faz Insert ou update
+@acessaDatabase
+def alteraInformacoes(query, parameters):
+    try:
+        cursor = conecta.cursor()
+        cursor.execute(query, parameters)
+        conecta.commit()
+        return True
+    except Exception as e:
+        print('erro ao executar query unica')
+        print(e)
+        raise
     
 if __name__ == '__main__':
     
     conexao_dados  = (
         "Driver={ODBC Driver 17 for SQL Server};"
-        "Server=00.000.000.00;"
-        "Database=your_database;"
+        "Server=00.00.000.000;"
+        "Database=YourDatabase;"
         "UID=user;" # usuario
-        "PWD=password" # password
+        "PWD=printer" # password
     )
     
-    query = "SELECT * FROM your_table"
-    dicionario = {'dados': conexao_dados, 'query': query}
-    resultado = buscaDadosMultiplos(dicionario)
+    query = "SELECT top(1) * FROM table where cpf = ?"
+    parameters = ["106.597.386-19"]
+    dicionario = {'dados': conexao_dados, 'query': query, 'parameters':parameters}
+    resultado, status = buscaDadoUnico(dicionario)
+    # resultado, status = buscaDadosMultiplos(dicionario)
     
-    print(resultado[0]['nome_coluna'])
-
+    print(resultado, status)
+    
